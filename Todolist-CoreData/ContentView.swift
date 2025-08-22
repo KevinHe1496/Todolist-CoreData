a57343a2864b043ea2c4bdce786242ab0694fa45
Kevin Heredia <kevin@ravecodesolutions.com>
2025-08-22 11:45:56 -0500

    Initial Commit

---
+++ Todolist-CoreData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todolist-CoreData
+//
+//  Created by Kevin Heredia on 22/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todolist-CoreData/Todolist_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todolist_CoreDataApp.swift
+//  Todolist-CoreData
+//
+//  Created by Kevin Heredia on 22/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Todolist_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
